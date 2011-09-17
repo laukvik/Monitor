eace63059d95fd0b51f444326838e0b5c9043963
Morten Laukvik <morten@laukvik.no>
2011-09-18 01:12:02 +0200

    Addede values for sensor and history
    Signed-off-by: Morten Laukvik <morten@laukvik.no>

--- src/org/laukvik/monitor/Monitor.sql
+++ src/org/laukvik/monitor/Monitor.sql
@@ -1,18 +1,26 @@
 
 CREATE TABLE SensorGroup(
 	sensorGroupID SERIAL,
-	title text CHECK (title <> ''),
-	description text,
-	PRIMARY KEY(sensorGroupID), UNIQUE(sensorGroupID,title)
-
+	title TEXT CHECK (title <> ''),
+	description TEXT,
+	PRIMARY KEY(sensorGroupID)
 );
 
 CREATE TABLE Sensor (
 	sensorID SERIAL NOT NULL,
 	sensorGroupID INT REFERENCES SensorGroup(sensorGroupID) NOT NULL,
-	title text CHECK (title <> ''),
-	description text,
-	className text NOT NULL CHECK(className<> ''), 
-	PRIMARY KEY(sensorID), UNIQUE(sensorID,title)
+	title TEXT CHECK (title <> ''),
+	description TEXT,
+	className TEXT NOT NULL CHECK(className<> ''), 
+        settings TEXT,
+        numberValue INT8,
+	PRIMARY KEY(sensorID)
 );
 
+CREATE TABLE History (
+	historyID SERIAL NOT NULL,
+        sensorID INT REFERENCES Sensor(sensorID) NOT NULL,
+        created TIMESTAMP DEFAULT now(),
+        numberValue INT8,
+	PRIMARY KEY(historyID)
+);
