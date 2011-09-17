b3e8b3bea1550d38646dd51b3bb24792403c342e
Morten Laukvik <morten@laukvik.no>
2011-09-18 01:13:47 +0200

    Refactored PluginExceptionAnalyzer to EmptyAnalyzer
    Signed-off-by: Morten Laukvik <morten@laukvik.no>

--- src/org/laukvik/monitor/EmptyAnalyzer.java
+++ src/org/laukvik/monitor/EmptyAnalyzer.java
@@ -10,18 +10,18 @@ import java.awt.Graphics2D;
  *
  * @author morten
  */
-public class PluginExceptionAnalyzer implements Analyzer {
+public class EmptyAnalyzer implements Analyzer {
 
     Sensor sensor;
     
-    public PluginExceptionAnalyzer( Sensor sensor ) {
+    public EmptyAnalyzer( Sensor sensor ) {
         this.sensor = sensor;
     }
 
     @Override
     public void paint( Graphics2D g, int width, int height ) {
         g.clearRect( 0, 0, width, height );
-        g.drawString( sensor.getTitle() + "(Configuration error)", 64, 20 );
+        g.drawString( "(Configuration error)", 64, 20 );
     }
 
     @Override
