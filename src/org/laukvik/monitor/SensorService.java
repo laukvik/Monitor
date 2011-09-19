b38175b236dcd209143577354789162a1e19ba9a
Morten Laukvik <morten@laukvik.no>
2011-09-20 00:37:21 +0200

    Commented out History entity because it wasn't working.
    Signed-off-by: Morten Laukvik <morten@laukvik.no>

--- src/org/laukvik/monitor/SensorService.java
+++ src/org/laukvik/monitor/SensorService.java
@@ -4,9 +4,6 @@
  */
 package org.laukvik.monitor;
 
-import org.laukvik.monitor.sensor.WebSite;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -27,18 +24,11 @@ public class SensorService {
     public SensorService() {
         emf = Persistence.createEntityManagerFactory("MonitorPU");
         emf.createEntityManager();
+        
         assert emf != null;  //Make sure injection went through correctly.
         em = emf.createEntityManager();
     }
-    
-//    public List<Analyzer> listSensorEnabled( SensorGroup sensorGroup ){
-//        List<Analyzer> items = new ArrayList<Analyzer>();
-//        for (Sensor s: listSensors(sensorGroup)){
-//            items.add( new WebSite(s) );
-//        }
-//        return items;
-//    }
-    
+
     public List<Sensor> listSensors( SensorGroup sensorGroup ){   
         List<Sensor> sensors = em.createNamedQuery("Sensor.findBySensorGroupid").setParameter( "sensorgroupid", sensorGroup ).getResultList();
         return sensors;
@@ -55,18 +45,24 @@ public class SensorService {
      * @param sensor 
      */
     public void createHistory( Sensor sensor ){
-        em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
-        History h = new History();
-        h.setSensorid(  sensor );
-        h.setCreated( new Date() ); 
-        h.setValue( sensor.getValue() );
-                
-        em.persist( h );
-        em.getTransaction().commit();     
+//        em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//        
+//        History h = new History();
+//        h.setSensorid(  sensor );
+//        h.setCreated( new Date() ); 
+//        h.setValue( sensor.getValue() );
+//                
+//        em.persist( h );
+//        em.getTransaction().commit();     
     }
 
+    public void addGroup( SensorGroup group ){
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist( group );
+        em.getTransaction().commit();        
+    }
     
     public void add( Sensor sensor ){
         em = emf.createEntityManager();
@@ -90,19 +86,34 @@ public class SensorService {
     }
     
     public static void main( String [] args ){
-        SensorService sm = new SensorService();
-
-        for (SensorGroup g : sm.listGroups()){
-            System.out.println( "Group: " + g.getTitle() );
+        SensorService srv = new SensorService();
+        
+        if (2 == 2){
+            SensorGroup group = new SensorGroup();
+            group.setTitle( "WebSites" );
+            group.setDescription( "" );
+            srv.addGroup( group );
             
-            for (Sensor s : g.getSensorList()){
-                System.out.println( "\tSensor: " + s.getTitle() );
+            Sensor s = new Sensor();
+            s.setTitle( "http://localhost" );
+            s.setClassname( "org.laukvik.monitor.sensor.WebSite" );
+            s.setSensorgroupid( group );
+            
+            srv.add( s );
+        }
 
-                for (History h : s.getHistoryList()){
-                    System.out.println( "\t\tHistory: " + h.getCreated() );
-                }   
-            }
-        }    
+
+//        for (SensorGroup g : sm.listGroups()){
+//            System.out.println( "Group: " + g.getTitle() );
+//            
+//            for (Sensor s : g.getSensorList()){
+//                System.out.println( "\tSensor: " + s.getTitle() );
+//
+//                for (History h : s.getHistoryList()){
+//                    System.out.println( "\t\tHistory: " + h.getCreated() );
+//                }   
+//            }
+//        }    
         
     }
     
