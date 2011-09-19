894c4a59051a9c57ca1c7484f38cfd9cb120a967
Morten Laukvik <morten@laukvik.no>
2011-09-20 00:34:20 +0200

    Removed unused imports
    Signed-off-by: Morten Laukvik <morten@laukvik.no>

--- src/org/laukvik/monitor/SensorTableCellRenderer.java
+++ src/org/laukvik/monitor/SensorTableCellRenderer.java
@@ -9,7 +9,6 @@ import java.awt.Component;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Paint;
 import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
