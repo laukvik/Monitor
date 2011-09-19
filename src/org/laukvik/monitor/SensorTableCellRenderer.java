e15fb7a9c5e46ea9347cfa7ebc8af07238d64fc3
Morten Laukvik <morten@laukvik.no>
2011-09-19 23:35:03 +0200

    Fixed design problems when rendering
    Signed-off-by: Morten Laukvik <morten@laukvik.no>

--- src/org/laukvik/monitor/SensorTableCellRenderer.java
+++ src/org/laukvik/monitor/SensorTableCellRenderer.java
@@ -4,10 +4,13 @@
  */
 package org.laukvik.monitor;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Paint;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
@@ -15,29 +18,45 @@ import javax.swing.table.TableCellRenderer;
  *
  * @author morten
  */
-public class SensorTableCellRenderer implements TableCellRenderer{
+public class SensorTableCellRenderer extends JComponent implements TableCellRenderer{
 
-    JComponent com;
+    Sensor sensor;
     
     public SensorTableCellRenderer() {
         super();
-        com = new JLabel();
+        setOpaque( true );
+        setBorder( null );
     }
     
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        Sensor sensor = (Sensor) value;
-        Graphics2D g = (Graphics2D) table.getGraphics();
-                
-        if (g == null){
-            System.out.println(  "null" );
-        } else {
-            g.setColor( isSelected ? table.getSelectionForeground() : table.getForeground() );
-            g.setBackground( isSelected ? table.getSelectionBackground() : table.getBackground() );
-            sensor.paint( g,  table.getWidth(), table.getRowHeight() );
-        }
-
-        return com;
+    public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        this.sensor = (Sensor) value;
+        setForeground( isSelected ? table.getSelectionForeground() : table.getForeground() );
+        setBackground( isSelected ? table.getSelectionBackground() : table.getBackground() );
+        return this;
     }
-    
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor( getBackground() );
+        g.fillRect( 0,0, getWidth(), getHeight() );
+        
+        g.setColor( new Color(33,38,43,90) );
+        g.fillRoundRect( 6,6, 48, 48, 10,10 );
+        
+        g.setColor( getForeground() );
+        sensor.paint( g, getWidth(), getHeight() );
+
+        
+        Graphics2D g2d = (Graphics2D)g;
+        GradientPaint p = new GradientPaint(
+            0, 0, new Color(255, 255, 255, 50), 0, getHeight(), new Color(0, 0, 0, 0)
+        );
+        g2d.setPaint( p );
+
+
+        g2d.fillRect(0, 0, getWidth(),  getHeight()/2);
+
+    }
+
 }
